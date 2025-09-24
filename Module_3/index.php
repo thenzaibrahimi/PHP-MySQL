0f3dfec8d5b28ec653c8586e674e7cd58cbaa909
thenzaibrahimi <ibrahimithenza08@gmail.com>
2025-09-24 20:43:00 +0200

    switch & loops

--- Module_3/index.php
+++ Module_3/index.php
@@ -49,4 +49,75 @@ else{
     echo "false";
 }
 
+echo "<br>";
+
+$age = 15;
+switch($age){
+    case($age >=0 && $age < 18):
+        echo "You are a minor(0-18) <br>";
+        break;
+    case($age >= 18 && $age <= 25):
+        echo"You are a young adult <br>";
+        break;
+    case($age >25):
+        echo "You are an adult <br>";
+        default:
+        echo "Ivalid age input <br>";
+        break; 
+}
+
+$day = "Monday";
+switch ($day){
+    case 'Monday':
+        echo "It's Monday";
+        break;
+
+         case 'Tuesday':
+        echo "It's Tuesday";
+        break;
+
+         case 'Wednesday':
+        echo "It's Wednesday";
+        break;
+
+         case 'Thursday':
+        echo "It's Thursday";
+        break;
+
+         case 'Friday':
+        echo "It's Friday";
+        break;
+
+         case 'Saturday':
+        echo "It's Saturday";
+        break;
+
+         case 'Sunday':
+        echo "It's Sunday";
+        break;
+
+        default:
+        echo "Ivalid date";
+        break;
+        
+}
+
+echo "<br>";
+
+
+$x = 1;
+
+while($x <= 5){
+    echo "The number is: $x <br>";
+    $x++;
+}
+
+echo "<br>";
+
+$y = 1;
+do{
+    echo "The number is: $y <br>";
+     $y++;
+}while($y <= 5)
+
 ?>
