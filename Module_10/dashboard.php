4178d93dd7c38579e9a35435dd903cafb3c6c0e5
thenzaibrahimi <ibrahimithenza08@gmail.com>
2025-11-03 20:43:39 +0100

    Fetching data

---
+++ Module_10/dashboard.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <style>
+        table, td, th{
+            border:1px solid black;
+            border-collapse: collapse;
+        }
+        td, th{
+            padding:10px 20px;
+        }
+    </style>
+</head>
+<body>
+   <?php
+   include_once('config.php');
+   $sql = "SELECT * FROM users";
+   $getUsers = $con->prepare($sql);
+   $getUsers->exceute();
+   $users=$getUsers->fetchAll();
+   ?> 
+   <br><br>
+
+   <table>
+        <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+        </thead>
+    <tbody>
+        <?php
+            foreach($users as $user){
+?> 
+    <tr>
+        <td> <?= $user['id'] ?> </td>
+        <td> <?= $user['name'] ?> </td>
+        <td> <?= $user['surname'] ?> </td>
+        <td> <?= $user['email'] ?></td>
+    </tr>
+    <?php
+            }
+    ?>        
+        
+    </tbody>
+    </table>
+    <a href="add.php">Add User</a>
+</body>
+</html>
